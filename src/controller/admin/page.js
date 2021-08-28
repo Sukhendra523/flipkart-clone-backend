ab45eb31008321723f1bf817a6649745df2c5885
Sukhendra Rajawat <523sukh@gmail.com>
2021-08-29 02:05:30 +0530

    Get Page is connected with frontend

--- src/controller/admin/page.js
+++ src/controller/admin/page.js
@@ -4,13 +4,13 @@ exports.createPage = (req, res) => {
     const { banners, products } = req.files;
     if (banners && banners.length > 0) {
       req.body.banners = banners.map((banner, index) => ({
-        img: `/public/${banner.filename}`,
+        img: `${process.env.API}/public/${banner.filename}`,
         navigateTo: `/bannerClicked?categoryId=${req.body.category}&type=${req.body.type}`,
       }));
     }
     if (products && products.length > 0) {
       req.body.products = products.map((product, index) => ({
-        img: `/public/${product.filename}`,
+        img: `${process.env.API}/public/${product.filename}`,
         navigateTo: `/productClicked?categoryId=${req.body.category}&type=${req.body.type}`,
       }));
     }
